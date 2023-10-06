aa467b5acde08dc76cfa20e73cdd6cae067a3bc0
Guillermo Rivas <grivas@pop-os.localdomain>
2023-10-06 08:47:49 -0600

    clase de octubre 6

--- app/src/main/java/mx/tec/clase1/FirebaseActivity.kt
+++ app/src/main/java/mx/tec/clase1/FirebaseActivity.kt
@@ -1,11 +1,15 @@
 package mx.tec.clase1
 
+import android.app.Activity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
@@ -32,6 +36,8 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 
 class FirebaseActivity : ComponentActivity() {
+
+    private lateinit var auth : FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -41,9 +47,21 @@ class FirebaseActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    FirebaseExample()
+                    FirebaseExample(auth, this)
                 }
             }
+        }
+
+        auth = Firebase.auth
+    }
+
+    public override fun onStart() {
+        super.onStart()
+
+        // lugar idóneo para verificar validez de usuario
+        val currentUser = auth.currentUser
+        if(currentUser == null){
+            // sin usuario - lánzalo a interfaz de login
         }
     }
 }
@@ -58,7 +76,7 @@ fun Greeting2(name: String, modifier: Modifier = Modifier) {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun FirebaseExample(){
+fun FirebaseExample(auth : FirebaseAuth, activity : Activity? = null){
 
     // add 2 properties
     var login by remember { mutableStateOf("") }
@@ -81,6 +99,112 @@ fun FirebaseExample(){
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
         )
 
+        Button(
+            onClick = {
+                if(activity != null){
+                    auth.signInWithEmailAndPassword(login, password)
+                        .addOnCompleteListener(activity){task ->
+                            if(task.isSuccessful){
+                                Toast.makeText(
+                                    activity,
+                                    "VALID LOGIN: ${auth.currentUser?.uid}",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            } else {
+                                Toast.makeText(
+                                    activity,
+                                    "INVALID LOGIN: ${task.exception?.message}",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
+                        }
+                }
+            }
+        ){
+            Text("Login")
+        }
+
+        Button(
+            onClick = {
+                // vamos a necesitar referencia a la actividad
+                // motivo: hay una task que necesita una actividad para ser escuchada
+                // null safety en kotlin -
+                // 1. safe call
+                // 2. utilizar if
+                if(activity != null){
+                    auth.createUserWithEmailAndPassword(login, password)
+                        .addOnCompleteListener(activity){task ->
+                            // RECUERDEN!
+                            // codigo asíncrono termina en un tiempo no determinado
+                            // AQUÍ ponemos lo que queremos que pase al terminar
+                            if(task.isSuccessful){
+                                Toast.makeText(activity, "SIGN UP SUCCESSFUL ${auth.currentUser?.uid}", Toast.LENGTH_SHORT).show()
+                            } else {
+                                Toast.makeText(activity, "ERROR: ${task.exception?.message}", Toast.LENGTH_SHORT).show()
+                            }
+                        }
+
+                    // aquí NO va lo que queremos que pase al terminar la ejecución
+                    // del código asíncrono
+
+                }
+            }
+        ){
+            Text("Sign up")
+        }
+
+        Button(
+            onClick = {
+                val db = Firebase.firestore
+
+                // crear los datos del nuevo registro
+                val doggy = hashMapOf(
+                    "name" to "Killer",
+                    "weight" to 2,
+                    "age" to 5
+                )
+
+                // guardar datos en DB
+                if(activity != null){
+
+                    db.collection("animals")
+                        .add(doggy)
+                        .addOnSuccessListener { newDoc ->
+                            Toast.makeText(activity, "NEW DOC: ${newDoc.id}", Toast.LENGTH_SHORT).show()
+                        }
+                        .addOnFailureListener{ exception ->
+                            Toast.makeText(activity, "ERROR: ${exception.message}", Toast.LENGTH_SHORT).show()
+                        }
+                }
+            }
+        ){
+            Text("Add record")
+        }
+
+        Button(
+            onClick = {
+
+                val db = Firebase.firestore
+
+                if(activity != null) {
+
+                    db.collection("animals")
+                        .get()
+                        .addOnSuccessListener { snapshot ->
+                            // ejemplito simple - iterar a través del snapshot
+                            for(doc in snapshot){
+                                Log.d("FIREBASE-TEST", "${doc.id} - ${doc.data}")
+                            }
+                        }
+                        .addOnFailureListener{ exception ->
+
+                            Log.wtf("FIREBASE-TEST", "${exception.message}")
+                        }
+                }
+            }
+        ){
+            Text("Query")
+        }
 
     }
 }
